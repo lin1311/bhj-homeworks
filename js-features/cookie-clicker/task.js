e5a6d6044da579d0c640a6ab44f8c61e1ce93ec8
Angelina Egorova <linaaa.egorova@gmail.com>
2024-11-17 18:18:44 +0300

    cookie clicker was solved

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,13 @@
+let cookieImg = document.getElementById ('cookie');
+let clicker__counter = document.getElementById('clicker__counter'); 
+let counter = 0;
+
+cookieImg.onclick = function() {
+  counter++;
+  clicker__counter.textContent = counter;
+  if (counter % 2 == 0) { 
+    cookieImg.width = '250'; 
+  } else{
+    cookieImg.width = '202';
+  } 
+}
